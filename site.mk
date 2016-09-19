10df929dad2daed65c3220e4f6d6d51fc9849d5a
FF Weinstrasse <rene@Renes-iMac.fritz.box>
2016-09-19 21:47:26 +0200

    Init new Repo

---
+++ site.mk
@@ -0,0 +1,143 @@
+##      gluon site.mk makefile example
+
+##      GLUON_SITE_PACKAGES
+#               specify gluon/openwrt packages to include here
+#               The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
+
+GLUON_SITE_PACKAGES := \
+       gluon-mesh-batman-adv-15 \
+       gluon-alfred \
+       gluon-respondd \
+       gluon-autoupdater \
+       gluon-config-mode-autoupdater \
+       gluon-config-mode-contact-info \
+       gluon-config-mode-core \
+       gluon-config-mode-geo-location \
+       gluon-config-mode-hostname \
+       gluon-config-mode-tunneldigger \
+       gluon-ebtables-filter-multicast \
+       gluon-ebtables-filter-ra-dhcp \
+       gluon-luci-admin \
+       gluon-luci-autoupdater \
+       gluon-luci-portconfig \
+       gluon-luci-wifi-config \
+       gluon-luci-private-wifi \
+       gluon-next-node \
+       gluon-mesh-vpn-tunneldigger \
+       gluon-radvd \
+       gluon-setup-mode \
+       gluon-status-page \
+       haveged \
+       iptables \
+       gluon-tunneldigger-watchdog \
+       gluon-migrate-vpn \
+       iwinfo \
+       #ffws-reboot \
+       #ffws-chkgw \
+       #ffws_keyupl
+       
+# support the USB stack
+USB_PACKAGES_BASIC := \
+       kmod-usb-core \
+       kmod-usb2
+
+# FAT32 Support for USB
+USB_PACKAGES_STORAGE := \
+       block-mount \
+       kmod-fs-ext4 \
+       kmod-fs-vfat \
+       kmod-usb-storage  \
+       kmod-usb-storage-extras  \
+       blkid  \
+       swap-utils  \
+       kmod-nls-cp1250  \
+       kmod-nls-cp1251  \
+       kmod-nls-cp437  \
+       kmod-nls-cp775  \
+       kmod-nls-cp850  \
+       kmod-nls-cp852  \
+       kmod-nls-cp866  \
+       kmod-nls-iso8859-1  \
+       kmod-nls-iso8859-13  \
+       kmod-nls-iso8859-15  \
+       kmod-nls-iso8859-2  \
+       kmod-nls-koi8r  \
+       kmod-nls-utf8
+
+USB_PACKAGES_NET := \
+       kmod-ath9k-htc \
+       kmod-usb-net \
+       kmod-usb-net-asix \
+       kmod-usb-net-asix-ax88179 \
+       kmod-usb-net-cdc-eem \
+       kmod-usb-net-cdc-ether \
+       kmod-usb-net-cdc-mbim \
+       kmod-usb-net-cdc-ncm \
+       kmod-usb-net-cdc-subset \
+       kmod-usb-net-dm9601-ether \
+       kmod-usb-net-hso \
+       kmod-usb-net-huawei-cdc-ncm \
+       kmod-usb-net-ipheth \
+       kmod-usb-net-kalmia \
+       kmod-usb-net-kaweth \
+       kmod-usb-net-mcs7830 \
+       kmod-usb-net-pegasus \
+       kmod-usb-net-qmi-wwan \
+       kmod-usb-net-rndis \
+       kmod-usb-net-rtl8150 \
+       kmod-usb-net-rtl8152 \
+       kmod-usb-net-sierrawireless \
+       kmod-usb-net-smsc95xx \
+       kmod-mii \
+       kmod-nls-base
+
+TOOLS_PACKAGES := \
+       usbutils
+
+
+ifeq ($(GLUON_TARGET),x86-generic)
+# support the usb stack on x86 devices
+# and add a few common USB NICs
+GLUON_SITE_PACKAGES += \
+       kmod-usb-hid \
+       $(USB_PACKAGES_BASIC) \
+       $(USB_PACKAGES_STORAGE) \
+       $(USB_PACKAGES_NET) \
+       $(TOOLS_PACKAGES)
+endif
+
+ifeq ($(GLUON_TARGET),x86-64)
+# support the usb stack on x86 devices
+# and add a few common USB NICs
+GLUON_SITE_PACKAGES += \
+       kmod-usb-hid \
+       $(USB_PACKAGES_BASIC) \
+       $(USB_PACKAGES_STORAGE) \
+       $(USB_PACKAGES_NET) \
+       $(TOOLS_PACKAGES)
+endif
+
+##      DEFAULT_GLUON_RELEASE
+#               version string to use for images
+#               gluon relies on
+#                       opkg compare-versions "$1" '>>' "$2"
+#               to decide if a version is newer or not.
+
+DEFAULT_GLUON_RELEASE := ffws-v016
+
+
+##      GLUON_RELEASE
+#               call make with custom GLUON_RELEASE flag, to use your own release version scheme.
+#               e.g.:
+#                       $ make images GLUON_RELEASE=23.42+5
+#               would generate images named like this:
+#                       gluon-ff%site_code%-23.42+5-%router_model%.bin
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+# Default priority for updates.
+GLUON_PRIORITY ?= 0
+
+# Languages to include
+GLUON_LANGS ?= en de
