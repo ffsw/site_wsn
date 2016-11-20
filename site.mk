00fe6e01e0f0afa6bf67a63a5167016ad00519dd
FF Weinstrasse <rene@freifunk-suedwest.de>
2016-11-20 14:41:58 +0100

    V18a -> V19

--- site.mk
+++ site.mk
@@ -145,7 +145,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwsn-v018a
+DEFAULT_GLUON_RELEASE := ffwsn-v019
 
 #       GLUON_RELEASE
 #               call make with custom GLUON_RELEASE flag, to use your own release version scheme.
