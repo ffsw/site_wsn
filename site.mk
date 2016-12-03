f2c812beebeb698220e5949cebd7c5912c152275
Tom <tsiener@gmx.de>
2016-12-03 23:52:33 +0100

    site.mk - v019f
    eigeness wifi-info package und verinfachter ssid-changer

--- site.mk
+++ site.mk
@@ -146,7 +146,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwsn-v019
+DEFAULT_GLUON_RELEASE := ffwsn-v019f
 
 #       GLUON_RELEASE
 #               call make with custom GLUON_RELEASE flag, to use your own release version scheme.
