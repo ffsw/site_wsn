6b834f95a772249d65144ed6b093ffd05ea25606
Tom <tsiener@gmx.de>
2019-02-08 15:04:14 +0100

    site.mk - v036a
    2018.2 with pr1641 (Archer C50v4 support)

--- site.mk
+++ site.mk
@@ -218,7 +218,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwsn-v036
+DEFAULT_GLUON_RELEASE := ffwsn-v036a
 
 
 #       GLUON_RELEASE
