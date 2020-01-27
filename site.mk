ac0a780e00f37090c13f7a11f0e81917a0468e9e
Tom <tsiener@gmx.de>
2020-01-27 01:15:22 +0100

    site.mk - v041

--- site.mk
+++ site.mk
@@ -192,7 +192,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwsn-v040a
+DEFAULT_GLUON_RELEASE := ffwsn-v041
 
 
 #       GLUON_RELEASE
