03583e0037adf3431599fb547c3ba3b7a2b84195
Tom <tsiener@gmx.de>
2020-02-16 09:43:07 +0100

    site.mk - v042 (gluon2020.1)

--- site.mk
+++ site.mk
@@ -192,7 +192,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwsn-v041
+DEFAULT_GLUON_RELEASE := ffwsn-v042
 
 
 #       GLUON_RELEASE
