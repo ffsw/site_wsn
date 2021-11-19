4724638dd1e123f349ec57d531d81ab7f18d370b
Tom <tsiener@gmx.de>
2021-11-19 12:33:46 +0100

    v046 mit gluon2021.1.1

--- site.mk
+++ site.mk
@@ -193,7 +193,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwsn-v045
+DEFAULT_GLUON_RELEASE := ffwsn-v046
 
 
 #       GLUON_RELEASE
